ce0973e9c3c023b29f744fe51e127ea45b9de326
Wil Black <wilblack21@gmail.com>
2021-12-05 12:24:48 -0800

    Initial commmit.

---
+++ app/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css';
+import Home from "./components/home/Home";
+import Results from "./components/results/Results";
+
+const Root = () => {
+    return (
+        
+        <Routes>
+        <Route path="/" element={  <Home/>} />
+           
+        <Route path="/results" element={ <Results/>} />
+            
+        </Routes>
+  )
+}
+
+
+const  App: FC = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Root />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/results/Results.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+
+
+const Results: FC = () => {
+    return (
+        <div>
+            <h1>Results</h1>
+        </div>
+    )
+}
+
+export default Results

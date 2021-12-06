bb93c659d28a39a1335a1480aa61bbbe4d913d07
Wil Black <wilblack21@gmail.com>
2021-12-05 17:34:04 -0800

    Adding solar prefix.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,16 +2,12 @@ import React, { FC } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from "./components/home/Home";
-import Results from "./components/results/Results";
 
 const Root = () => {
     return (
         
         <Routes>
-        <Route path="/solar" element={  <Home/>} />
-           
-        <Route path="/results" element={ <Results/>} />
-            
+          <Route path="/" element={  <Home/>} />
         </Routes>
   )
 }
@@ -20,7 +16,7 @@ const Root = () => {
 const  App: FC = () => {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="solar/">
         <Root />
       </BrowserRouter>
     </div>
